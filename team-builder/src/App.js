c5b00c448aef694cf00b61971251dfff397e95a8
MichaelHMods <mikeguadalupe@gmail.com>
2019-09-19 09:35:10 -0500

    AppTest.jsx done. Need to create TeamCard now

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import AppTest from "../src/Components/AppTest";
+import AppTest from "./AppTest";
 
 
 function App() {
--- team-builder/src/AppTest.jsx
+++ team-builder/src/AppTest.jsx
@@ -3,19 +3,30 @@ import TeamForm from './Components/TeamForm';
 
 
 const AppTest = () => {
-    const [member, setMember] = useState({
+    const [members, setMembers] = useState({
         name: "",
         email: "",
         role: "", 
     });
 
-    const addNewMember
+    const addNewMember = member =>{
+        const newMember = {
+            id: Date.now(),
+            //date now will denote a unique id number
+            name: member.name,
+            email: member.email,
+            role: member.role,
+        };
+        setMembers([...members, newMember]);
+    }
     return (
+
         <div>
             <header>
                 <h1 className="title">Build Your Team HERE!!!</h1>
             </header>
-            <TeamForm/>
+            <TeamForm addNewMember={addNewMember}/>
+            <TeamCard members={members}/>
 
         </div>
     )
